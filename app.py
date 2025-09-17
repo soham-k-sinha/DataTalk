4989523e11cf99534d5a4e17d937fd4870542018
Soham Sinha <sohamkundan@gmail.com>
2025-09-17 00:21:19 -0400

    Working Gemini API calls with FastAPI post request

--- app.py
+++ app.py
@@ -1,8 +1,27 @@
 from fastapi import FastAPI
+from dotenv import load_dotenv
+import os
+from google import genai
 
 app = FastAPI()
 
 
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+def root():
+    return {}
+
+
+@app.post("/call_api/")
+async def api_call(prompt: str):
+    load_dotenv()
+
+    API_KEY = os.getenv("GEMINI_API_KEY")
+
+    client = genai.Client(api_key=API_KEY)
+
+    response = client.models.generate_content(
+        model="gemini-2.5-flash-lite",
+        contents=f"{prompt}",
+    )
+
+    return {"output": f"{response.text}"}
